537f76eac463bc2173496f052c43cf677eab73d2
Parth Mehta <parthpm06@gmail.com>
2024-08-03 23:40:36 -0400

    "commit"

--- MyProgram.java
+++ MyProgram.java
@@ -1,10 +1,9 @@
-// import java.util.Scanner; // Import the Scanner class
-
-public class MyProgram {
-    public static void main(String[] args) {
+public class MyProgram 
+{
+    public static void main(String[] args) 
+    {
         menu myMenu = new menu();
         myMenu.runner();
-
     }
 
 }
